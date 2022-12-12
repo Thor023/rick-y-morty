070a915c8693545517654f39984d05bb09468e29
Thor023 <felipe.castaneda84@gmail.com>
2022-12-12 19:07:18 -0300

    trabajando en botones de filtros

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FilterBTN from '../FilterBTN';
 
 const Status = () => {
+    let status = ["Alive","Dead","Unknown"]
   return (
     <div className="accordion-item">
     <h2 className="accordion-header" id="headingThree">
@@ -11,7 +12,10 @@ const Status = () => {
     </h2>
     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div className="accordion-body">
-        <FilterBTN/>
+        {status.map((items, index)=> (
+            <FilterBTN key={index} name="status" index={index} items={items}/>
+        ))};
+
       </div>
     </div>
   </div>
--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const FilterBTN = () => {
+const FilterBTN = ({name, index, items}) => {
   return (
     <div>
-    <div className="form-check">
-        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-            <label class="btn btn-outline-primary" for="flexRadioDefault1">
-                Single toggle
-            </label>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="radio"
+          name={name}
+          id={`${name}-${index}`}
+ 
+        />
+        <label class="btn btn-outline-primary" for={`${name}-${index}`}>
+            {items}
+        </label>
+      </div>
     </div>
-<div className="form-check">
-  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-  <label className="btn btn-outline-primary" for="flexRadioDefault2">
-    Default checked radio
-  </label>
-</div>
-</div>
-  )
-}
+  );
+};
 
-export default FilterBTN
+export default FilterBTN;
