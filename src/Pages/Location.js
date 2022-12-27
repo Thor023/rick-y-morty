73f448e946518b19e53bc41bd3f997f26731864e
Thor023 <felipe.castaneda84@gmail.com>
2022-12-27 14:32:37 -0300

    modificaciones para hacer la pagina responsiva

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -40,11 +40,11 @@ const Location = () => {
         </h6>
       </div>
       <div className='row'>
-        <div className='col-3'>
+        <div className='col-lg-3 col-12'>
           <h4 className='text-center mb-4'>Escoge un lugar</h4>
           <InputGroup setId={setId} name="Lugar de origen" total = {126} />
         </div>
-        <div className='col-8'>
+        <div className='col-lg-8 col-12'>
           <div className='row'>
             <Cards page="/location/" results={results} />
           </div>
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactPaginate from 'react-paginate';
-// import styles from "./Pagination.module.scss"
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
 
+  let [width, setWidth] = useState(window.innerWidth);
+
+  let updateDimension = ()=>{
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(()=>{
+    window.addEventListener("resize", updateDimension);
+    return () => window.removeEventListener("resize", updateDimension);
+  },[]);
+
   return (
+    <>
+    <style jsx>
+      {`
+      @media (max-width: 768px){
+        .next, .prev {
+          display: none;
+        }
+        .pagination{
+          font-size: 14px;
+        }
+      }
+      `}
+
+    </style>
     <ReactPaginate 
     className= "pagination justify-content-center gap-4 my-4"
-    forcePage={pageNumber===1? 0 : pageNumber -1}
-    breakLabel="..."
-    nextLabel = "Siguiente"
-    previousLabel = "Anterior"
-    nextclassName ='btn btn-primary'
-    previousclassName='btn btn-primary '
-    pageclassName='page-item'
-    pageLinkclassName='page-link '
-    activeclassName='active'
+    nextLabel = "Next"
+    forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+    previousLabel = "Prev"
+    nextClassName ='btn btn-primary next'
+    previousClassName='btn btn-primary prev'
+    pageClassName='page-item'
+    pageLinkClassName='page-link'
+    marginPagesDisplayed={width < 576 ? 1 : 2 }
+    pageRangeDisplayed={width < 576 ? 1 : 2 }
+    activeClassName='active'
     onPageChange={(data) => {
-      setPageNumber(data.selected + 1)
+      setPageNumber(data.selected + 1);
     }}
-    pageCount={info?.pages}/>
+    pageCount={info?.pages}
+    />
+    </>
   );
 };
 
